15f58cbd06b31be5106229dffc2c75a1abc33367
Sergey Shvager <sergey.shvager@home24.de>
2016-03-23 00:06:06 +0100

    Optimized build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
+
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.BABEL_ENV = TARGET;
 
 const TARGET = process.env.npm_lifecycle_event;
-process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
     app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
+    build: path.join(__dirname, 'build'),
+    style: path.join(__dirname, 'app/main.css')
 };
+
+const pkg = require('./package.json');
 
 const common = {
     entry: {
-        app: PATHS.app
+        app: PATHS.app,
+        style: PATHS.style
     },
 
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         loaders: [{
-            test: /\.css$/,
-            loaders: ['style', 'css'],
-            include: PATHS.app
-        }, {
           test: /\.jsx?$/,
           loaders: ['babel?cacheDirectory'],
           include: PATHS.app
@@ -32,15 +38,30 @@ const common = {
     },
     resolve: {
       extensions: ['', '.js', '.jsx']
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'node_modules/html-webpack-template/index.ejs',
+            title: 'Kanban app',
+            appMountId: 'app',
+            inject: false
+        })
+    ]
+
 };
 
 
 if (TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
+        module: {
+            loaders: [{
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            }]
+        },
         devServer: {
-            contentBase: PATHS.build,
             historyApiFallback: true,
             hot: true,
             inline: true,
@@ -58,6 +79,45 @@ if (TARGET === 'start' || !TARGET) {
     });
 }
 
-if (TARGET === 'build') {
-    module.exports = merge(common, {});
+if (TARGET === 'build' || TARGET === 'stats') {
+    module.exports = merge(common, {
+        entry: {
+            vendor: Object.keys(pkg.dependencies).filter(function(v) {
+                return v !== 'alt-utils'
+            })
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].[chunkhash].js',
+            chunkFilename: '[chunkhash].js'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: PATHS.app
+                }
+            ]
+        },
+
+        plugins: [
+            new CleanPlugin([PATHS.build], {
+                verbose: false
+            }),
+            new ExtractTextPlugin('[name].[chunkhash].css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest']
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
 }
